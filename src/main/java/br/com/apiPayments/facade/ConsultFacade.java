a70b8e3fb8a9938a7eccb443c29d244e5d4e69a7
PedroFrancholi <francholidearaujo@gmail.com>
2026-03-18 23:55:26 -0300

    Consult_Account

--- src/main/java/br/com/apiPayments/facade/ConsultFacade.java
+++ src/main/java/br/com/apiPayments/facade/ConsultFacade.java
@@ -1,11 +1,14 @@
 package br.com.apiPayments.facade;
 
 import br.com.apiPayments.exception.GenericException;
+import br.com.apiPayments.model.AccountModel;
 import br.com.apiPayments.model.ComponentModel;
 import br.com.apiPayments.model.HistoricalModel;
+import br.com.apiPayments.repository.AccountRepository;
 import br.com.apiPayments.repository.ComponentRepository;
 import br.com.apiPayments.repository.HistoricalRepository;
 import br.com.apiPayments.util.LogUtil;
+import br.com.apiPayments.web.dto.response.AccountResponseDto;
 import br.com.apiPayments.web.dto.response.ComponentResponseDto;
 import br.com.apiPayments.web.dto.response.HistoricalResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +25,9 @@ public class ConsultFacade {
 
     @Autowired
     private HistoricalRepository historicalRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
 
     public List<ComponentResponseDto> consultComponents() {
         try{
@@ -120,6 +126,71 @@ public class ConsultFacade {
                 .dtCreatedAt(historicalModel.getDtCreatedAt())
                 .build();
 
+        }catch (GenericException e){
+            LogUtil.error(this.getClass(), "Client error: " + e.getMessage(), e);
+            throw e;
+        } catch (IllegalArgumentException e) {
+            LogUtil.error(this.getClass(), "Invalid argument: " + e.getMessage(), e);
+            throw new GenericException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            LogUtil.error(this.getClass(), "Error during create component.",e);
+            throw new GenericException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public AccountResponseDto consultAccountByNrAccount(Integer nrAccount) {
+        try{
+            AccountModel account = accountRepository.findByNrAccount(nrAccount)
+                    .orElseThrow(() -> new GenericException("Account with number "+nrAccount+" not found!", HttpStatus.NOT_FOUND));
+
+            return AccountResponseDto.builder()
+                    .cdAgency(account.getCdAgency())
+                    .nrAccount(account.getNrAccount())
+                    .vlAmount(account.getVlAmount())
+                    .dtCreatedAt(account.getDtCreatedAt())
+                    .component(ComponentResponseDto.builder()
+                                .cdDocument(account.getComponent().getCdDocument())
+                                .nmPerson(account.getComponent().getNmPerson())
+                                .tpPerson(account.getComponent().getTpPerson())
+                                .dsEmail(account.getComponent().getDsEmail())
+                                .build())
+                    .build();
+
+        }catch (GenericException e){
+            LogUtil.error(this.getClass(), "Client error: " + e.getMessage(), e);
+            throw e;
+        } catch (IllegalArgumentException e) {
+            LogUtil.error(this.getClass(), "Invalid argument: " + e.getMessage(), e);
+            throw new GenericException(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }catch (Exception e){
+            LogUtil.error(this.getClass(), "Error during create component.",e);
+            throw new GenericException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public List<AccountResponseDto> consultAccounts() {
+        try{
+
+            List<AccountModel> accounts = accountRepository.findAll();
+
+            if(accounts.isEmpty()){
+                throw new GenericException("No accounts found!", HttpStatus.NOT_FOUND);
+            }
+
+            return  accounts.stream()
+                    .map(account -> AccountResponseDto.builder()
+                            .cdAgency(account.getCdAgency())
+                            .nrAccount(account.getNrAccount())
+                            .vlAmount(account.getVlAmount())
+                            .dtCreatedAt(account.getDtCreatedAt())
+                            .component(ComponentResponseDto.builder()
+                                    .cdDocument(account.getComponent().getCdDocument())
+                                    .nmPerson(account.getComponent().getNmPerson())
+                                    .tpPerson(account.getComponent().getTpPerson())
+                                    .dsEmail(account.getComponent().getDsEmail())
+                                    .build())
+                            .build())
+                    .toList();
         }catch (GenericException e){
             LogUtil.error(this.getClass(), "Client error: " + e.getMessage(), e);
             throw e;
--- src/main/java/br/com/apiPayments/web/controller/ConsultController.java
+++ src/main/java/br/com/apiPayments/web/controller/ConsultController.java
@@ -1,8 +1,10 @@
 package br.com.apiPayments.web.controller;
 
 import br.com.apiPayments.facade.ConsultFacade;
+import br.com.apiPayments.web.dto.response.AccountResponseDto;
 import br.com.apiPayments.web.dto.response.ComponentResponseDto;
 import br.com.apiPayments.web.dto.response.HistoricalResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,22 +22,38 @@ public class ConsultController {
     private ConsultFacade consultFacade;
 
     @GetMapping("/component")
+    @Operation(summary = "Consult all components.", description = "Consult all components that registered.")
     private ResponseEntity<List<ComponentResponseDto>> consultComponents() {
         return ResponseEntity.status(HttpStatus.OK).body(consultFacade.consultComponents());
     }
 
     @GetMapping("/component/{cdDocument}")
+    @Operation(summary = "Consult a component.", description = "Consult component by document code.")
     private ResponseEntity<ComponentResponseDto> consultComponentById(@PathVariable String cdDocument) {
         return ResponseEntity.status(HttpStatus.OK).body(consultFacade.consultComponentByCdDocument(cdDocument));
     }
 
     @GetMapping("/historical")
+    @Operation(summary = "Consult all historical.", description = "Consult all historical that registered.")
     private ResponseEntity<List<HistoricalResponseDto>> consultHistoricals() {
         return ResponseEntity.status(HttpStatus.OK).body(consultFacade.consultHistoricals());
     }
 
     @GetMapping("/historical/{idHistorical}")
+    @Operation(summary = "Consult an historical.", description = "Consult historical by id historical.")
     private ResponseEntity<HistoricalResponseDto> consultHistoricalById(@PathVariable Integer idHistorical) {
         return ResponseEntity.status(HttpStatus.OK).body(consultFacade.consultHistoricalById(idHistorical));
+    }
+
+    @GetMapping("/account/{nrAccount}")
+    @Operation(summary = "Consult an account.", description = "Consult account by account number.")
+    private ResponseEntity<AccountResponseDto> consultAccountById(@PathVariable Integer nrAccount) {
+        return ResponseEntity.status(HttpStatus.OK).body(consultFacade.consultAccountByNrAccount(nrAccount));
+    }
+
+    @GetMapping("/account")
+    @Operation(summary = "Consult all accounts.", description = "Consult all accounts that registered.")
+    private ResponseEntity<List<AccountResponseDto>> consultAccounts() {
+        return ResponseEntity.status(HttpStatus.OK).body(consultFacade.consultAccounts());
     }
 }
